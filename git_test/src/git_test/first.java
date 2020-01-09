aea0b67efd6ae405185d1c8a5c48a59eb66fe17a
seunghunleee <seunghunne@gmail.com>
2020-01-10 04:33:40 +0900

    테스트용 입니다.

---
+++ git_test/src/git_test/first.java
@@ -0,0 +1,11 @@
+package git_test;
+
+public class first {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("이클립스 테스트 용입니다.");
+		System.out.println("테스트 1");
+	}
+
+}
